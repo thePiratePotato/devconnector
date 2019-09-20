15b5be5c4445713b75eef4caccec72f00beb56de
vuhoangninh1998 <hoangninh.vu@my.jcu.edu.au>
2019-09-20 12:08:05 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('API Runnning'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
