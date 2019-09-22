c451b9807ee500e85c64266c4de8c8656fc28172
vuhoangninh1998 <hoangninh.vu@my.jcu.edu.au>
2019-09-22 09:41:54 +0800

    4th commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 
-
-// @route GET api/profile
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => res.send('profile router'));
+// @route GET api/profile/me
+// @desc Get current users profile route
+// @access Private
+router.get('/', auth, (req, res) => res.send('profile router'));
 
 module.exports = router;
