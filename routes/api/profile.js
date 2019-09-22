38a88e4f836f6fca578924f7206f12bbe8d19ab5
vuhoangninh1998 <hoangninh.vu@my.jcu.edu.au>
2019-09-22 11:58:08 +0800

    6th commit, working on profile user/ creating, updating, bla bla

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -37,6 +37,72 @@ router.post('/', [authenticate, [
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-    });
 
+        // pulling all the stuff out from the req.body
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            skills,
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkedin
+        } = req.body;
+
+        // build project onject
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if (company) profileFields.company = company;
+        if (website) profileFields.website = website;
+        if (location) profileFields.location = location;
+        if (bio) profileFields.bio = bio;
+        if (status) profileFields.status = status;
+        if (githubusername) profileFields.githubusername = githubusername;
+        if (skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+        }
+        // print skills out
+        // console.log(profileFields.skills);
+
+
+        // build social Object
+        profileFields.social = {};
+        if (twitter) profileFields.social.twitter = twitter;
+        if (facebook) profileFields.social.facebook = facebook;
+        if (instagram) profileFields.social.instagram = instagram;
+        if (linkedin) profileFields.social.linkedin = linkedin;
+        if (youtube) profileFields.social.youtube = youtube;
+
+
+
+        // print social
+        // console.log(profileFields.social);
+        // testing on postman
+        // res.send("hello");
+
+        try {
+            // find by user
+            let profile = await Profile.findOne({ user: req.user.id });
+            // look for a profile, if there is one
+
+            if (profile) {
+                // update Profile
+                profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true });
+                return res.json(profile);
+            }
+            profile = new Profile(profileFields);
+            await profile.save();
+            // send back the profile
+            res.json(profile);
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    });
 module.exports = router;
