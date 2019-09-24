950555acd2371983da28132dfc5c2b4b43a25ebc
vuhoangninh1998 <hoangninh.vu@my.jcu.edu.au>
2019-09-24 21:13:16 +0800

    14th commit, react, redux

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 
 app.get('/', (req, res) => res.send('API Runnning'));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
