6858fbf814c87b78ae1fdaea39b06a6f2fd7facd
vuhoangninh1998 <hoangninh.vu@my.jcu.edu.au>
2019-09-24 08:32:50 +0800

    13th commit, like and unlike?

--- models/Post.js
+++ models/Post.js
@@ -16,7 +16,7 @@ const PostSchema = new Schema({
     avatar: {
         type: String
     },
-    links: [{
+    likes: [{
         user: {
             type: Schema.Types.ObjectId,
             ref: 'users'
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -98,5 +98,48 @@ router.delete('/:id', authenticate, async(req, res) => {
         res.status(500).send('Server Error');
     }
 });
+// @route PUT api/posts/like/:id
+// @desc LIKE a post
+// @access Private, must login to delete
+router.put('/like/:id', authenticate, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        // Check if the post has already been like
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Post already liked' });
+
+        }
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+// @route PUT api/posts/unlike/:id
+// @desc UNLIKE a post
+// @access Private, must login to delete
+router.put('/unlike/:id', authenticate, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        // Check if the post has already been like
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: 'Post not liked yet' });
+
+        }
+        // GET remove likes
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(removeIndex, 1);
+
+        await post.save();
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
