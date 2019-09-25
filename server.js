9130464a44fb6aa1a73a66b36b9813428ebd4f62
vuhoangninh1998 <hoangninh.vu@my.jcu.edu.au>
2019-09-25 11:11:17 +0800

    The final final commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 const BodyParser = require('body-parser');
-
+const path = require('path');
 // Connect to MongoDB
 connectDB();
 // Init Middleware to use bodyparser to get data from req.body
@@ -15,8 +15,13 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
-
-app.get('/', (req, res) => res.send('API Runnning'));
+// sever static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build)'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
